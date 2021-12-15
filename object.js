f7d4c15b00f4d619c0b15ed1e1a80b6407798044
davincecode <hyperscript.ca@gmail.com>
2021-12-15 16:28:43 +0000

    Nose School

--- object.js
+++ object.js
@@ -2,6 +2,6 @@ let pizza = {
   toppings: ["cheese", "bacon", "pepperoni", "ham"],
   crust: "deep dish",
   size: ["small", "medium", "large"],
-  sauce: ["traditional", "creamy garlic"],
+  sauce: ["traditional", "creamy garlic", "sausy"],
 };
 console.log(pizza);
